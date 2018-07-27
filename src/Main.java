125207b3e0ab18c1e3008d872f639f3828a2f393
AIJakel <32111839+AIJakel@users.noreply.github.com>
2018-07-26 22:32:21 -0400

    worked on prob generation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
-    static double incidentRate = .25;
-    static double findingCancerGivenCancer = .75;
+    static double incidentRate = .124;
+    static double findingCancerGivenCancer = .87;
     static double findingNoCancerGivenNoCancer = 1;
+    //these are constants
+    static double live1yearnoCancer = 0.996614;
+    static double live1yearwithCancer = .63;
+    static double live5yearnoCancer = 0.98765;
+    static double live5yearwithCancer = .87;
+    static double live15yearnoCancer = 0.98765;
+    static double live15yearwithCancer = .50;
+    static double live25yearnoCancer = 0.974518;
+    static double live25yearwithCancer = .50;
 
     public static void main(String[] args) {
 
@@ -20,15 +29,33 @@ public class Main {
 
     //generates the probabilites for 1 test over the 10 year period
     private static void ProbGenerator_1Test(int years) {
-        double probCancer = 1 - Math.pow(1 - incidentRate, years);
+        //create the nodes
+        Node previous = null;
+        for (int i = 1; i <= years; i++)
+        {
+            //generates the probabilities -> turn into function
+            double probCancer = 1 - Math.pow(1 - incidentRate, years);
+            //get test results
+            double probTestPos = probCancer * findingCancerGivenCancer;
+            double probTestNeg = probCancer * (1 - findingCancerGivenCancer) + 1 - probCancer;
+            //true neg and pos results
+            double probTrueNeg = (1 - probCancer) / probTestNeg;
+            double probFalseNeg = 1 - probTrueNeg;
+            //for defferal probs note the live 1 yr thing is the next roots prior prob need.
+            double probLive1yrWithCancer = (1 - probCancer)*live1yearnoCancer + probCancer * live1yearwithCancer;
+            double probDiein1yrWithCancer = 1-probLive1yrWithCancer;
 
-        //get test results
-        double probTestPos = probCancer * findingCancerGivenCancer;
-        double probTestNeg = probCancer * (1 - findingCancerGivenCancer) + 1 - probCancer;
-        double probTrueNeg = (1 - probCancer) / probTestNeg;
-        double probFalseNeg = 1 - probTrueNeg;
-        double probLive1yrWithCancer = 0;
-        double probDiein1yrWithCancer = 0;
+            //create descision nodes
+            if (i == 1)
+            {
+                Node year1 = new Node(1);
+                year1.
+            }
+            else if(previous != null)
+            {
+                Node
+            }
+        }
     }
 
     /*
